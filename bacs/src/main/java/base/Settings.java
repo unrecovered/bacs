2c0637688e6bec3d937635dd2900c2ff97bcffd9
Valery Ilychev (Sarutobi) <ilychev@gmail.com>
2017-03-08 22:27:52 +0400

    Базовый класс настроек

---
+++ bacs/src/main/java/base/Settings.java
@@ -0,0 +1,26 @@
+package base;
+
+/**
+ * Created by valera on 08.03.17.
+ */
+public class Settings {
+    int cores;
+    int dimension;
+    int scale;
+    int maxIterations;
+    int actLim;
+    int relSense;
+    int gainBase;
+    boolean lumus;
+
+    int strength;
+    int mutagen;
+    int end;
+
+    int cmdMove;
+    int cmdTurn;
+    int cmdEat;
+    int cmdGain;
+    int cmdAttack;
+    int cmdObserve;
+}
